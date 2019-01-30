31809ac608352d8bd6806d958310fad677e8fbfd
Danilo Henrique <danilo.2500@gmail.com>
2019-01-30 11:00:21 -0200

    Adicionado model de fundos

---
+++ Teste-iOS-BTG/Models/Funds.swift
@@ -0,0 +1,36 @@
+//
+//  Funds.swift
+//  Teste-iOS-BTG
+//
+//  Created by Danilo Henrique on 30/01/19.
+//  Copyright © 2019 BTG Pactual. All rights reserved.
+//
+
+import Foundation
+
+struct Funds: Codable {
+    
+    let product: String
+    let categoryDescription: String
+    let profitability: FundProfitability
+    let minimumInitialInvestment: Float
+    let detail: FundDetail
+    let begin: String
+    let netEquity: Double
+    
+}
+
+class FundProfitability: Codable {
+    
+    let month: Double
+    let year: Double
+    let twelveMonths: Double
+    
+}
+
+class FundDetail: Codable {
+    
+    let manager: String
+    let investimentQuota: String
+    
+}
