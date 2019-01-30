9e4cdab8acbcef758c066ec3333d2f72642ebf43
Danilo Henrique <danilo.2500@gmail.com>
2019-01-30 14:16:02 -0200

    setup View controller inicial

--- Teste-iOS-BTG/AppDelegate.swift
+++ Teste-iOS-BTG/AppDelegate.swift
@@ -16,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        setupInitialViewController()
+        
         return true
+    }
+    
+    func setupInitialViewController() {
+        let storyboard = UIStoryboard(name: "FundsInvestments", bundle: nil)
+        window?.rootViewController = storyboard.instantiateInitialViewController()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
